05b1fdec7ca20e646bba1e8229216e10fbf825bc
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-08-01 16:52:32 -0400

    Changed index.php

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
-
-	<!-- Le styles -->
-	<link href="css/bootstrap.css" rel="stylesheet">
 	<style type="text/css">
 	body {
 		padding-top: 60px;
@@ -17,7 +14,8 @@
         max-width: 700px;
       }
 	</style>
-	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<!-- Le Bootstrap styles -->
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
