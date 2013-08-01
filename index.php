b392e5e80f2b8996261e3d8f57b5075136d5cc32
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-08-01 16:51:25 -0400

    Added new index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<html lang="en"><head>
+	<meta charset="utf-8">
+	<title>CRoPS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Le styles -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<style type="text/css">
+	body {
+		padding-top: 60px;
+		padding-bottom: 40px;
+	}
+  .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+	</style>
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <![endif]-->
+
+    <link type="text/css" rel="stylesheet" href="chrome-extension://cpngackimfmofbokmjmljamhdncknpmg/style.css"><script type="text/javascript" charset="utf-8" src="chrome-extension://cpngackimfmofbokmjmljamhdncknpmg/page_context.js"></script><style type="text/css"></style></head>
+    <body screen_capture_injected="true">
+     <div class="container-narrow">
+      <div class="masthead">
+       <ul class="nav nav-pills pull-right">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="http://github.com/wallarelvo/camgaze.js">Source</a></li>
+      </ul>
+      <h3 class="muted">CRoPS</h3>
+    </div>
+    <div class="hero-unit">
+      <img src="http://aw204.host.cs.st-andrews.ac.uk/CRoPS/page-0.jpg">
+     <h1>CRoPS</h1>
+     <p>Combined Roadmaps and Potentials for Swarms</p>
+     <p><a href="Taros2013.zip" class="btn btn-primary btn-large">Download source »</a></p>
+
+   </div>
+   <div class="footer">
+    <p>
+        Creators: 
+        <a href="http://www.linkedin.com/pub/alexander-wallar/68/985/b87">
+            Alex Wallar
+        </a>
+    </p>
+    <p>Website by: <a href="http://www.thundersun.net">Valentin Tunev</a></p>
+  </div>
+</div>
+
+
+
+</body></html>
