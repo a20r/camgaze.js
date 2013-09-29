60671235e0e52645311e853816aeafefd0dd8466
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-09-29 21:47:31 +0100

    Making changes to the code which is viewable on the server

--- js/camgaze.js
+++ js/camgaze.js
@@ -1497,7 +1497,7 @@ camgaze.EyeTracker.prototype = {
 //////////////////////////////////////////////////////////////
 
 camgaze.EyeFilter = function () {
-	this.movAvgLength = 20;
+	this.movAvgLength = 35;
 	this.movAvgDict = {};
 	this.MovingAveragePoints = camgaze.structures.MovingAveragePoints;
 	this.origin = new camgaze.structures.Point(0, 0);
