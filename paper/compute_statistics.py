70aecf35a7f2fc0d136f451b25ccc99c3098d3c3
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-10-02 21:23:59 +0100

    Changed the structure of the test

--- paper/compute_statistics.py
+++ paper/compute_statistics.py
@@ -1,5 +1,7 @@
 
 import json
+import math
+import pprint
 
 def getStats():
 
@@ -9,18 +11,32 @@ def getStats():
     xMean = 0
     yMean = 0
     distMean = 0
+
     for userStat in jsonObj:
         xMean += (float(userStat["xMean"]) / len(jsonObj))
         yMean += (float(userStat["yMean"]) / len(jsonObj))
         distMean += (float(userStat["distMean"]) / len(jsonObj))
 
+    xVar = 0
+    yVar = 0
+    distVar = 0
+
+    for userStat in jsonObj:
+        xVar += (pow(float(userStat["xMean"] - xMean), 2) / len(jsonObj))
+        yVar += (pow(float(userStat["yMean"] - yMean), 2) / len(jsonObj))
+        distVar += (pow(float(userStat["distMean"] - distMean), 2) / len(jsonObj))
+
+
     return {
             "xMean" : xMean,
             "yMean" : yMean,
             "distMean" :distMean,
+            "xStd" : math.sqrt(xVar),
+            "yStd" : math.sqrt(yVar),
+            "distStd" : math.sqrt(distVar),
             "totalTrials" : len(jsonObj)
     }
 
 if __name__ == "__main__":
 
-    print getStats()
+    pprint.pprint(getStats())
